bd2b3acec6f85e6c638166dce2139585b2bf7eee
Robear Selwans <robear.selwans@outlook.com>
2022-01-03 00:02:16 +0200

    Added EV_VA_OPT
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- ev_macros.h
+++ ev_macros.h
@@ -116,6 +116,15 @@
 #define EV_VA_ARGS_RSEQ_N() 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
 /*!
+ * \brief Similar functionality to `__VA_OPT__`
+ * Used as follows:
+ * ```c
+ * #define TEST(...) EV_VA_OPT(__VA_ARGS__)("Not ")"Empty"
+ * ```
+ */
+#define EV_VA_OPT(...) __EV_MACRO_IF_ELSE(__EV_MACRO_NOT(EV_VA_ARGS_NARG(__VA_ARGS__)))()
+
+/*!
  * \brief Calls the passed macro for each element.
  * \note Maximum number of elements to iterate over is 64 elements.
  */
